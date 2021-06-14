da3a9630df79d617576b00136109fb566d219970
marnysan111 <o-masato@outlook.jp>
2021-06-14 09:17:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { TextField,Button,Grid } from '@material-ui/core/';
 
-function App() {
+export default function Practice() {
+    return (
+      <React.Fragment>
+
+        <Grid container justify="center">
+          <h1>Todo</h1>
+        </Grid>
+
+        <Grid container>
+
+          <Grid item xs={4}>
+            <h2>グリッドのこっち松</h2>
+          </Grid>
+
+          <Grid item xs={8}>
+
+            <form onSubmit={e => {
+              console.log(e.target.value)
+            }}>
+              <Grid item>
+                <TextField label="Title" variant="outlined" id="title"/>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary" type="submit">
+                  Send
+                </Button>
+              </Grid>
+            </form>
+          </Grid>
+
+        </Grid>
+
+      </React.Fragment>
+    )
+};
+
+const todoList = (props) => {
+  console.log(props.title)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <p>{props.title}</p>
+  )
 }
-
-export default App;
